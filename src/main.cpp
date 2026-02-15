a1c67073eb72b0c28446563962a36f1ee7bbf522
Kamil Wróblewski <k.wroblewski54@gmail.com>
2026-02-15 14:16:03 +0100

    Add raybli 5.5 submodule, update cmakelists and make test window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "raylib.h"
 
 int main(int argc, char** argv)
 {
-  std::cout << "Hello world!" << std::endl;
+  InitWindow(800, 600, "Hello World");
+  while(!WindowShouldClose())
+  {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Hello World!", 350, 280, 20, BLACK);
+    EndDrawing();
+  }
+
+  CloseWindow();
   return 0;
 }
